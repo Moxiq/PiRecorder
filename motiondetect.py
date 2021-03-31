fb41cc26c1bb8e7eef329821f7189518e06e8329
Fabian Mathisson <tehtaq@gmail.com>
2021-04-01 00:34:47 +0200

    Update motiondetect.py

--- motiondetect.py
+++ motiondetect.py
@@ -5,13 +5,14 @@ import numpy as np
 
 class MotionDetection:
     def __init__(self):
+        # Tries to find background by looking at history of frames
         self.fbgb = cv.createBackgroundSubtractorMOG2(300, 400, True)
         self.framecount = 0
 
     def process_frame(self, frame):
         self.framecount += 1
 
-        # Resize the image
+        # Resize the image for faster processing
         resized_frame = cv.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Get the foreground mask
@@ -20,12 +21,14 @@ class MotionDetection:
         # Count all the non zero pixels within the mask
         count = np.count_nonzero(fgmask)
 
+        # Decides when a detection is to be triggered
         if self.framecount > 1 and count > 5000:
             print("Movement")
         
         cv.imshow("Resized Frame", resized_frame)
         cv.imshow("Mask", fgmask)
 
+        # Close window when 'q' is pressed
         if cv.waitKey(1) & 0xFF == ord('q'):
             cv.destroyAllWindows()
 
